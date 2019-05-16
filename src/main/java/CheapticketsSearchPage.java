049c6f72693246f79a78137ae3878c1e20988aae
Lucas Zarza <lucas.zarza@gmail.com>
2019-05-16 15:11:28 -0300

    Excercise 1 - 8 steps finished without bonus

---
+++ src/main/java/CheapticketsSearchPage.java
@@ -0,0 +1,83 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
+
+import java.security.Key;
+
+public class CheapticketsSearchPage extends BasePage {
+
+    @FindBy (how = How.NAME, using = "destination")
+    private WebElement destiny;
+
+    @FindBy (how = How.NAME, using = "startDate")
+    private WebElement startDate;
+
+    @FindBy (how = How.NAME, using = "endDate")
+    private WebElement endDate;
+
+    @FindBy (how = How.ID, using = "hotel-1-adults-hlp")
+    private Select adults;
+
+    @FindBy (how = How.ID, using = "hotel-1-children-hlp")
+    private Select child;
+
+    @FindBy (how = How.ID, using = "hotel-1-age-select-1-hlp")
+    private Select age;
+
+    @FindBy (how = How.CLASS_NAME, using = "gcw-submit")
+    private WebElement search;
+
+
+    public CheapticketsSearchPage(WebDriver driver){
+        super(driver);
+    }
+
+    public WebElement getDestiny(){
+        return destiny;
+    }
+
+    public WebElement getStartDate(){
+        return startDate;
+    }
+
+    public WebElement getEndDate(){
+        int i = 10;
+
+        do{
+            endDate.sendKeys(Keys.BACK_SPACE);
+            i--;
+        }while (i!=0);
+
+        return endDate;
+    }
+
+    public WebElement getSearch(){
+        return search;
+    }
+
+    public void setAdults(String n){
+
+        adults = new Select(getDriver().findElement(By.id("hotel-1-adults-hlp")));
+        adults.selectByVisibleText(n);
+
+    }
+
+    public void setChild(String n){
+
+        child = new Select(getDriver().findElement(By.id("hotel-1-children-hlp")));
+        child.selectByVisibleText(n);
+
+    }
+
+    public void setAge(String n){
+
+        age = new Select(getDriver().findElement(By.id("hotel-1-age-select-1-hlp")));
+        age.selectByVisibleText(n);
+
+    }
+
+}
--- src/test/java/Exercise_1.java
+++ src/test/java/Exercise_1.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Exercise_1 {
 
@@ -10,6 +15,8 @@ public class Exercise_1 {
     private String url = "https://www.cheaptickets.com/";
     private CheapticketsHomePage cheapT;
     private WebDriverWait wait;
+    private CheapticketsSearchPage searchPage;
+    private String destino = "Miami Beach";
 
     @BeforeClass
     public void testSetUp() {
@@ -22,6 +29,18 @@ public class Exercise_1 {
     public void hotelSearch(){
         driver.navigate().to(url);
         cheapT.getHotel().click();
+        searchPage = new CheapticketsSearchPage(driver);
+        searchPage.getDestiny().sendKeys(destino);
+        searchPage.getStartDate().sendKeys("08/22/2019");
+        WebElement endDate = searchPage.getEndDate();
+        endDate.sendKeys("09/10/2019");
+        searchPage.setAdults("4");
+        searchPage.setChild("1");
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("hotel-1-age-select-1-hlp"))));
+        searchPage.setAge("7");
+        searchPage.getSearch().click();
+
+
     }
 
 }
